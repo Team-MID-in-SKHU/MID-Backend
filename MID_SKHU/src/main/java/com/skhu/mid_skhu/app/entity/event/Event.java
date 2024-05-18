c6a23979a27a0d1e6d6dd6aaa8a5c16d896515fb
woogym <kwj9294@gmail.com>
2024-05-18 17:42:16 +0900

    [feature] #39 delete unused code of domain v2

--- MID_SKHU/src/main/java/com/skhu/mid_skhu/app/entity/event/Event.java
+++ MID_SKHU/src/main/java/com/skhu/mid_skhu/app/entity/event/Event.java
@@ -23,7 +23,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
