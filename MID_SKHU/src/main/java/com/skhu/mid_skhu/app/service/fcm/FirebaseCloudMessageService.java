2f2cbdbbefa589e5f5dbcbdd845460f9ba2da0cf
woogym <kwj9294@gmail.com>
2024-05-26 22:50:51 +0900

    [feature] #46 FirebaseMessageService logic

---
+++ MID_SKHU/src/main/java/com/skhu/mid_skhu/app/service/fcm/FirebaseCloudMessageService.java
@@ -0,0 +1,78 @@
+package com.skhu.mid_skhu.app.service.fcm;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.common.net.HttpHeaders;
+import com.google.gson.JsonParseException;
+import com.skhu.mid_skhu.app.dto.fcm.FcmMessage;
+import lombok.RequiredArgsConstructor;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class FirebaseCloudMessageService {
+
+    @Value("${fcm.api_url}")
+    private String API_URL;
+
+    @Value("${fcm.fcm_config}")
+    private String encodedFirebaseServiceKey;
+
+    private final ObjectMapper objectMapper;
+
+    public void sendMessageTo(String targetToken, String title, String body) throws IOException {
+        String message = makeMessage(targetToken, title, body);
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .post(requestBody)
+                .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
+                .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println(response.body().string());
+    }
+
+    private String makeMessage(String targetToken, String title, String body) throws JsonParseException, JsonProcessingException {
+        FcmMessage fcmMessage = FcmMessage.builder()
+                .message(FcmMessage.Message.builder()
+                        .token(targetToken)
+                        .notification(FcmMessage.Notification.builder()
+                                .title(title)
+                                .body(body)
+                                .image(null)
+                                .build()
+                        )
+                        .build()).validateOnly(false).build();
+
+        return objectMapper.writeValueAsString(fcmMessage);
+    }
+
+    private String getAccessToken() throws IOException{
+        byte[] decodedServiceKey = Base64.getDecoder().decode(encodedFirebaseServiceKey);
+
+        GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(new ByteArrayInputStream(decodedServiceKey))
+                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+
+        googleCredentials.refreshIfExpired();
+        return googleCredentials.getAccessToken().getTokenValue();
+    }
+}
