82252fc8ecc1219f80a548e2dce94bdfaa6ebeb2
woogym <kwj9294@gmail.com>
2024-05-31 19:39:23 +0900

    [feature] #46 modify repository for EventPushAlarmSchedulerService

--- MID_SKHU/src/main/java/com/skhu/mid_skhu/app/repository/StudentRepository.java
+++ MID_SKHU/src/main/java/com/skhu/mid_skhu/app/repository/StudentRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
@@ -16,4 +17,6 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     Optional<Student> findByStudentNo(String studentNo);
 
     List<Student> findAllByCategory(InterestCategory category);
+
+    List<Student> findByCategoryIn(Set<InterestCategory> categories);
 }
