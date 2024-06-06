0484a7794633fa9fdc7985d59b474f74f781f83b
woogym <kwj9294@gmail.com>
2024-06-06 22:34:32 +0900

    [feature] #64 : S3FileService

---
+++ MID_SKHU/src/main/java/com/skhu/mid_skhu/app/service/event/S3FileService.java
@@ -0,0 +1,50 @@
+package com.skhu.mid_skhu.app.service.event;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.skhu.mid_skhu.global.exception.ErrorCode;
+import com.skhu.mid_skhu.global.exception.model.CustomException;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@Component
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class S3FileService {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String uploadImageFile(MultipartFile imageFile, String directory) throws IOException {
+        String imageFileName = sanitizeImageFileName(imageFile.getOriginalFilename());
+        String imageFileUrl = "https://" + bucket + "/" + directory + "/" + imageFileName;
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(imageFile.getContentType());
+        metadata.setContentLength(imageFile.getSize());
+
+        amazonS3Client.putObject(bucket, directory + "/" + imageFileName, imageFile.getInputStream(), metadata);
+
+        return imageFileUrl;
+    }
+
+    private String sanitizeImageFileName(String imageFileName) {
+        String encodedImageFileName;
+        try {
+            encodedImageFileName = URLEncoder.encode(imageFileName, StandardCharsets.UTF_8.toString());
+            return encodedImageFileName.replaceAll("/", "");
+        } catch (UnsupportedEncodingException e) {
+            throw new CustomException(ErrorCode.FAIL_ENCODING_IMAGE_FILE_NAME,
+                    ErrorCode.FAIL_ENCODING_IMAGE_FILE_NAME.getMessage());
+        }
+    }
+}
--- MID_SKHU/src/main/java/com/skhu/mid_skhu/global/exception/ErrorCode.java
+++ MID_SKHU/src/main/java/com/skhu/mid_skhu/global/exception/ErrorCode.java
@@ -27,6 +27,7 @@ public enum ErrorCode {
     SMS_CERTIFICATION_MISMATCH_EXCEPTION(HttpStatus.BAD_REQUEST, "인증번호가 일치하지 않습니다."),
     SMS_CERTIFICATION_SEND_MISSING_EXCEPTION(HttpStatus.BAD_REQUEST, "인증번호 발송에 실패했습니다."),
     SMS_CERTIFICATION_VERIFY_FAIL_EXCEPTION(HttpStatus.BAD_REQUEST, "문자 인증에 실패했습니다."),
+    FAIL_ENCODING_IMAGE_FILE_NAME(HttpStatus.BAD_REQUEST, "파일명 인코딩에 실패했습니다."),
 
     // 401 Unauthorized
     UNAUTHORIZED_EMAIL_EXCEPTION(HttpStatus.UNAUTHORIZED, "이메일 인증이 되지 않은 유저"),
