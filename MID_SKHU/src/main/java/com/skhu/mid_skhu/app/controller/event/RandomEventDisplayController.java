71319773759e2840567558db4c5467c4984d5b80
Kang Duoh <kduoh99@gmail.com>
2024-06-09 18:42:54 +0900

    [refactor] #65 Separated responsibilities in EventDisplayController and EventSearchService

--- MID_SKHU/src/main/java/com/skhu/mid_skhu/app/controller/event/RandomEventDisplayController.java
+++ MID_SKHU/src/main/java/com/skhu/mid_skhu/app/controller/event/RandomEventDisplayController.java
@@ -1,7 +1,7 @@
 package com.skhu.mid_skhu.app.controller.event;
 
 import com.skhu.mid_skhu.app.dto.event.responseDto.EventSearchResponseDto;
-import com.skhu.mid_skhu.app.service.event.EventDisplayService;
+import com.skhu.mid_skhu.app.service.event.RandomEventDisplayService;
 import com.skhu.mid_skhu.global.common.dto.ApiResponseTemplate;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -19,22 +19,22 @@ import java.util.List;
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @Tag(name = "행사/이벤트", description = "행사/이벤트를 조회하는 API 그룹")
 @RequestMapping("/api/v1/event/display")
-public class EventDisplayController {
+public class RandomEventDisplayController {
 
-    private final EventDisplayService eventDisplayService;
+    private final RandomEventDisplayService randomEventDisplayService;
 
-    @GetMapping
+    @GetMapping("/random")
     @Operation(
-            summary = "신청중/마감임박 이벤트 조회",
-            description = "신청중 이벤트와 마감임박 이벤트를 각각 하나씩 조회하는 API입니다.",
+            summary = "랜덤 이벤트 조회",
+            description = "랜덤으로 두 가지 이벤트를 조회하는 API입니다.",
             responses = {
-                    @ApiResponse(responseCode = "200", description = "신청중/마감임박 이벤트 조회 성공"),
+                    @ApiResponse(responseCode = "200", description = "랜덤 이벤트 조회 성공"),
                     @ApiResponse(responseCode = "403", description = "URL문제 or 관리자 문의"),
                     @ApiResponse(responseCode = "500", description = "토큰 문제 or 관리자 문의")
             }
     )
-    public ResponseEntity<ApiResponseTemplate<List<EventSearchResponseDto>>> displayOneOngoingAndOneEndingEvent() {
-        ApiResponseTemplate<List<EventSearchResponseDto>> dataList = eventDisplayService.displayOneOngoingAndOneEndingEvent();
+    public ResponseEntity<ApiResponseTemplate<List<EventSearchResponseDto>>> displayRandomEvents() {
+        ApiResponseTemplate<List<EventSearchResponseDto>> dataList = randomEventDisplayService.displayRandomEvents();
         return ResponseEntity.status(dataList.getStatus()).body(dataList);
     }
 }
--- MID_SKHU/src/main/java/com/skhu/mid_skhu/app/service/event/PartialEventSearchService.java
+++ MID_SKHU/src/main/java/com/skhu/mid_skhu/app/service/event/PartialEventSearchService.java
@@ -1,11 +1,9 @@
 package com.skhu.mid_skhu.app.service.event;
 
-import com.skhu.mid_skhu.app.dto.event.requestDto.EventSearchRequestDto;
 import com.skhu.mid_skhu.app.dto.event.responseDto.EventSearchResponseDto;
 import com.skhu.mid_skhu.app.entity.event.Event;
 import com.skhu.mid_skhu.app.entity.interest.InterestCategory;
 import com.skhu.mid_skhu.app.repository.EventRepository;
-import com.skhu.mid_skhu.app.repository.specification.EventSpecification;
 import com.skhu.mid_skhu.global.common.dto.ApiResponseTemplate;
 import com.skhu.mid_skhu.global.exception.ErrorCode;
 import com.skhu.mid_skhu.global.exception.model.CustomException;
@@ -14,34 +12,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.security.Principal;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
-public class EventSearchService {
+public class PartialEventSearchService {
 
     private final EventRepository eventRepository;
 
     @Transactional(readOnly = true)
-    public ApiResponseTemplate<List<EventSearchResponseDto>> searchEvents(Principal principal,
-                                                                          EventSearchRequestDto eventSearchRequestDto) {
-
-        List<InterestCategory> categories = InterestCategory.convertToCategoryList(eventSearchRequestDto.getCategoryList());
-
-        List<Event> events = eventRepository.findAll(EventSpecification.withDynamicQuery(
-                eventSearchRequestDto.getTitle(),
-                eventSearchRequestDto.getStartAt(),
-                eventSearchRequestDto.getEndAt(),
-                categories
-        ));
-
+    public ApiResponseTemplate<List<EventSearchResponseDto>> partialSearchEvents(String partialTitle) {
+        List<Event> events = eventRepository.findByTitleContaining(partialTitle);
         if (events.isEmpty()) {
             throw new CustomException(ErrorCode.NOT_FOUND_EVENT_DATA_EXCEPTION,
                     ErrorCode.NOT_FOUND_EVENT_DATA_EXCEPTION.getMessage());
         }
-
 
         List<EventSearchResponseDto> responseDtoList = events.stream()
                 .map(event -> EventSearchResponseDto.builder()
