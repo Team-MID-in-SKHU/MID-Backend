1944825bd13d5cdf318518daada581ab7d616665
woogym <kwj9294@gmail.com>
2024-06-11 02:23:25 +0900

    [feature] #64 add EventUpdateRequestDto

---
+++ MID_SKHU/src/main/java/com/skhu/mid_skhu/app/dto/event/requestDto/EventUpdateRequestDto.java
@@ -0,0 +1,19 @@
+package com.skhu.mid_skhu.app.dto.event.requestDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class EventUpdateRequestDto {
+
+    private String title;
+    private String description;
+    private String eventLocation;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private List<String> interestCategories;
+}
