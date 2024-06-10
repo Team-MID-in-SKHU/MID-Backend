64b18451e3261c62a8cbbd421054bd25ba7617fb
woogym <kwj9294@gmail.com>
2024-06-10 18:47:22 +0900

    [feature] #64 modify S3Congig line

--- MID_SKHU/src/main/java/com/skhu/mid_skhu/global/config/S3Config.java
+++ MID_SKHU/src/main/java/com/skhu/mid_skhu/global/config/S3Config.java
@@ -2,6 +2,7 @@ package com.skhu.mid_skhu.global.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,7 +21,9 @@ public class S3Config {
     @Bean
     public AmazonS3Client amazonS3Client() {
         BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+
+        return (AmazonS3Client) AmazonS3ClientBuilder
+                .standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
