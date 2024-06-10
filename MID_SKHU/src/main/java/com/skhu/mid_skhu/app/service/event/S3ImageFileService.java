1d675f716908fc67a403b5eb8d46004fdffa623d
woogym <kwj9294@gmail.com>
2024-06-10 19:34:32 +0900

    [feature] #64 modify S3ImageService interface return type object

--- MID_SKHU/src/main/java/com/skhu/mid_skhu/app/service/event/S3ImageFileService.java
+++ MID_SKHU/src/main/java/com/skhu/mid_skhu/app/service/event/S3ImageFileService.java
@@ -1,11 +1,12 @@
 package com.skhu.mid_skhu.app.service.event;
 
+import com.skhu.mid_skhu.global.util.GetS3Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
 
 public interface S3ImageFileService {
-    String uploadImageFile(MultipartFile imageFile, String directory) throws IOException;
-    List<String> uploadImageFiles(List<MultipartFile> imageFiles, String directory);
+    GetS3Resource uploadSingleImageFile(MultipartFile imageFile, String directory) throws IOException;
+    List<GetS3Resource> uploadImageFiles(List<MultipartFile> imageFiles, String directory);
 }
